9747d22d7be344849db9c80c1b44da25645035c4
elfsvet <stepanmatysik@gmail.com>
2022-08-11 14:49:36 -0400

    from snapshot

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
   const {
     image,
     name,
@@ -14,18 +16,12 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const [state, dispatch] = useStoreContext();
-
-  const { cart } = state;
+  const { cart } = state
 
   const addToCart = () => {
-    // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-
-    // if there was a match, call UPDATE with a new purchase quantity
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
     if (itemInCart) {
       dispatch({
-        // update doesnt work in indexdb
         type: UPDATE_CART_QUANTITY,
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,19 +30,23 @@ const typeDefs = gql`
     orders: [Order]
   }
 
+  type Checkout {
+    session: ID
+  }
+
   type Auth {
     token: ID
     user: User
   }
 
   type Query {
-  categories: [Category]
-  products(category: ID, name: String): [Product]
-  product(_id: ID!): Product
-  user: User
-  order(_id: ID!): Order
-  checkout(products: [ID]!): Checkout
-}
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
@@ -51,10 +55,6 @@ const typeDefs = gql`
     updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
-
-  type Checkout {
-  session: ID
-}
 `;
 
 module.exports = typeDefs;
